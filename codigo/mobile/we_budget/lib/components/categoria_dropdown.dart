dff485e44ba3b66392c4738deb8a989c290c8ce9
Nataniel Geraldo Mendes Peixoto <nataniel.geraldo@yahoo.com.br>
2022-10-18 22:16:50 -0300

    Ajuste ToggleSwitch

--- codigo/mobile/we_budget/lib/components/categoria_dropdown.dart
+++ codigo/mobile/we_budget/lib/components/categoria_dropdown.dart
@@ -16,35 +16,43 @@ class _DropdownButtonExampleState extends State<DropdownButtonExample> {
   Widget build(BuildContext context) {
     return InputDecorator(
       decoration: InputDecoration(
-        contentPadding: EdgeInsets.symmetric(
-            horizontal: 20.0, vertical: 15.0),
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 20.0, vertical: 7.0),
         labelText: ('Categoria'),
         border: OutlineInputBorder(
           borderSide: BorderSide(width: 0.8, color: Colors.grey), //<-- SEE HERE
           borderRadius: BorderRadius.circular(50.0),
         ),
       ),
-      child: DropdownButton<String>(
-        value: dropdownValue,
-        //icon: const Icon(Icons.arrow_downward),
-        elevation: 16,
-        //style: const TextStyle(color: Colors.deepPurple),
-        //underline: Container(
-         // height: 2,
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          splashColor: Colors.transparent, // <- Here
+          highlightColor: Colors.transparent, // <- Here
+          hoverColor: Colors.transparent,
+        ),
+        child: DropdownButton<String>(
+          value: dropdownValue,
+          focusColor: Colors.transparent,
+          //icon: const Icon(Icons.arrow_downward),
+          elevation: 16,
+          //style: const TextStyle(color: Colors.deepPurple),
+          //underline: Container(
+          // height: 2,
           //color: Colors.deepPurpleAccent,
-        //),
-        onChanged: (String? value) {
-          // This is called when the user selects an item.
-          setState(() {
-            dropdownValue = value!;
-          });
-        },
-        items: list.map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-        }).toList(),
+          //),
+          onChanged: (String? value) {
+            // This is called when the user selects an item.
+            setState(() {
+              dropdownValue = value!;
+            });
+          },
+          items: list.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
--- codigo/mobile/we_budget/lib/pages/registrar_transacao_page.dart
+++ codigo/mobile/we_budget/lib/pages/registrar_transacao_page.dart
@@ -94,154 +94,169 @@ class _TransacaoFormPageState extends State<TransacaoFormPage> {
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     return Scaffold(
-              appBar: AppBar(
-                title: Text('Registrar transação'),
-                backgroundColor: Colors.blueAccent,
-              ),
-              body: Padding(
-                padding: const EdgeInsets.all(30),
-                child: Form(
-                  key: _formKey,
-                  child: ListView(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 74.0, top: 0.0, right: 74.0, bottom: 20.0),
-                        child: ToggleSwitch(
-                          minWidth: 140.0,
-                          minHeight: 25.0,
-                          cornerRadius: 20.0,
-                          activeBgColors: const [
-                            [Colors.blueAccent],
-                            [Colors.blueAccent],
-                          ],
-                          borderWidth: 5,
-                          activeFgColor: Colors.white,
-                          inactiveBgColor: Colors.grey,
-                          inactiveFgColor: Colors.white,
-                          initialLabelIndex: 1,
-                          totalSwitches: 2,
-                          labels: const ['Despesa', 'Receita'],
-                          radiusStyle: true,
-                          onToggle: (index) {
-                            print('switched to: $index');
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 1.0, top: 0.0, right: 1.0, bottom: 0.0),
-                        child: TextFormField(
-                          initialValue: _formData['name']?.toString(),
-                          decoration: InputDecoration(labelText: 'Nome',
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(width: 0.8, color: Colors.grey), //<-- SEE HERE
-                              borderRadius: BorderRadius.circular(50.0),
-                            ),),
-                          textInputAction: TextInputAction.next,
-                          onFieldSubmitted: (_) {
-                            FocusScope.of(context).requestFocus(_priceFocus);
-                          },
-                          onSaved: (name) => _formData['name'] = name ?? '',
-                          validator: (_name) {
-                            final name = _name ?? '';
-
-                            if (name.trim().isEmpty) {
-                              return 'Nome é obrigatório.';
-                            }
-
-                            if (name.trim().length < 3) {
-                              return 'Nome precisa no mínimo de 3 letras.';
-                            }
-
-                            return null;
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 1.0, top: 25.0, right: 1.0, bottom: 0.0),
-                        child: DropdownButtonExample()
-                        ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 1.0, top: 25.0, right: 1.0, bottom: 0.0),
-                        child: DatePicker(),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 1.0, top: 25.0, right: 1.0, bottom: 0.0),
-                        child: TextFormField(
-                          autofocus: false,
-                          initialValue: _formData['price']?.toString(),
-                          decoration: InputDecoration(labelText: 'R\$ 00,00',
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(width: 0.8, color: Colors.grey), //<-- SEE HERE
-                              borderRadius: BorderRadius.circular(50.0),
-                            ),
-                          ),
-                          textInputAction: TextInputAction.next,
-                          focusNode: FocusNode(),
-                          keyboardType: const TextInputType.numberWithOptions(
-                            decimal: true,
-                            signed: true,
-                          ),
-                          // onFieldSubmitted: (_) {
-                          //   FocusScope.of(context)
-                          //       .requestFocus(_descriptionFocus);
-                          // },
-                          onSaved: (price) =>
-                              _formData['price'] = double.parse(price ?? '0'),
-                          validator: (_price) {
-                            final priceString = _price ?? '';
-                            final price = double.tryParse(priceString) ?? -1;
-
-                            if (price <= 0) {
-                              return 'Informe um preço válido.';
-                            }
-
-                            return null;
-                          },
-                        ),
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.only(left: 1.0, top: 25.0, right: 1.0, bottom: 0.0),
-                          child: DropdownButtonPagamentoExample()
-                      ),
-                      Container(
-                        padding: EdgeInsetsDirectional.only(top: 20.0),
-                        child: ElevatedButton(
-                          // onPressed: _submit,
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.grey,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            fixedSize: const Size(290, 50),
-                          ),
-                          onPressed: () {},
-                          child: Text('Buscar Localização'),
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsetsDirectional.only(top: 15.0),
-                        child: ElevatedButton(
-                          // onPressed: _submit,
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.blueAccent,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 100,
-                              vertical: 20,
-                            ),
-                            fixedSize: const Size(290, 50),
-                            //backgroundColor: Theme.of(context).colorScheme.primary,
-                          ),
-                          onPressed: () {},
-                          child: Text('Registrar'),
-                        ),
-                      ),
-                    ],
-                  ),
+      appBar: AppBar(
+        title: Text('Registrar transação'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              Container(
+                margin: const EdgeInsetsDirectional.only(bottom: 20.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ToggleSwitch(
+                      minWidth: 140.0,
+                      minHeight: 25.0,
+                      cornerRadius: 20.0,
+                      activeBgColors: const [
+                        [Colors.blueAccent],
+                        [Colors.blueAccent],
+                      ],
+                      borderWidth: 5,
+                      activeFgColor: Colors.white,
+                      inactiveBgColor: Colors.grey,
+                      inactiveFgColor: Colors.white,
+                      initialLabelIndex: 1,
+                      totalSwitches: 2,
+                      labels: const ['Despesa', 'Receita'],
+                      radiusStyle: true,
+                      onToggle: (index) {
+                        print('switched to: $index');
+                      },
+                    ),
+                  ],
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 1.0, top: 0.0, right: 1.0, bottom: 0.0),
+                child: TextFormField(
+                  initialValue: _formData['name']?.toString(),
+                  decoration: InputDecoration(
+                    labelText: 'Nome',
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                          width: 0.8, color: Colors.grey), //<-- SEE HERE
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                  ),
+                  textInputAction: TextInputAction.next,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_priceFocus);
+                  },
+                  onSaved: (name) => _formData['name'] = name ?? '',
+                  validator: (_name) {
+                    final name = _name ?? '';
+
+                    if (name.trim().isEmpty) {
+                      return 'Nome é obrigatório.';
+                    }
+
+                    if (name.trim().length < 3) {
+                      return 'Nome precisa no mínimo de 3 letras.';
+                    }
+
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: 1.0, top: 25.0, right: 1.0, bottom: 0.0),
+                child: DropdownButtonExample(),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 1.0, top: 25.0, right: 1.0, bottom: 0.0),
+                child: DatePicker(),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 1.0, top: 25.0, right: 1.0, bottom: 0.0),
+                child: TextFormField(
+                  autofocus: false,
+                  initialValue: _formData['price']?.toString(),
+                  decoration: InputDecoration(
+                    labelText: 'R\$ 00,00',
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                          width: 0.8, color: Colors.grey), //<-- SEE HERE
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                  ),
+                  textInputAction: TextInputAction.next,
+                  focusNode: FocusNode(),
+                  keyboardType: const TextInputType.numberWithOptions(
+                    decimal: true,
+                    signed: true,
+                  ),
+                  // onFieldSubmitted: (_) {
+                  //   FocusScope.of(context)
+                  //       .requestFocus(_descriptionFocus);
+                  // },
+                  onSaved: (price) =>
+                      _formData['price'] = double.parse(price ?? '0'),
+                  validator: (_price) {
+                    final priceString = _price ?? '';
+                    final price = double.tryParse(priceString) ?? -1;
+
+                    if (price <= 0) {
+                      return 'Informe um preço válido.';
+                    }
+
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(
+                      left: 1.0, top: 25.0, right: 1.0, bottom: 0.0),
+                  child: DropdownButtonPagamentoExample()),
+              Container(
+                padding: EdgeInsetsDirectional.only(top: 20.0),
+                child: ElevatedButton(
+                  // onPressed: _submit,
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.grey,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    fixedSize: const Size(290, 50),
+                  ),
+                  onPressed: () {},
+                  child: Text('Buscar Localização'),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsetsDirectional.only(top: 15.0),
+                child: ElevatedButton(
+                  // onPressed: _submit,
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blueAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 100,
+                      vertical: 20,
+                    ),
+                    fixedSize: const Size(290, 50),
+                    //backgroundColor: Theme.of(context).colorScheme.primary,
+                  ),
+                  onPressed: () {},
+                  child: Text('Registrar'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
