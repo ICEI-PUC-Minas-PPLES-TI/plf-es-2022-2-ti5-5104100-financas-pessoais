93a1a47741b3b5be0174fbcccf14754c70cecafe
Pedro Barcelos <pbarcelos56@icloud.com>
2022-10-19 18:11:22 -0300

    feat: Atualização Tela Registrar Transações

--- codigo/mobile/we_budget/lib/components/categoria_dropdown.dart
+++ codigo/mobile/we_budget/lib/components/categoria_dropdown.dart
@@ -30,28 +30,30 @@ class _DropdownButtonExampleState extends State<DropdownButtonExample> {
           highlightColor: Colors.transparent, // <- Here
           hoverColor: Colors.transparent,
         ),
-        child: DropdownButton<String>(
-          value: dropdownValue,
-          focusColor: Colors.transparent,
-          //icon: const Icon(Icons.arrow_downward),
-          elevation: 16,
-          //style: const TextStyle(color: Colors.deepPurple),
-          //underline: Container(
-          // height: 2,
-          //color: Colors.deepPurpleAccent,
-          //),
-          onChanged: (String? value) {
-            // This is called when the user selects an item.
-            setState(() {
-              dropdownValue = value!;
-            });
-          },
-          items: list.map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
+        child: DropdownButtonHideUnderline(
+          child: DropdownButton<String>(
+            value: dropdownValue,
+            focusColor: Colors.transparent,
+            //icon: const Icon(Icons.arrow_downward),
+            elevation: 16,
+            //style: const TextStyle(color: Colors.deepPurple),
+            //underline: Container(
+            // height: 2,
+            //color: Colors.deepPurpleAccent,
+            //),
+            onChanged: (String? value) {
+              // This is called when the user selects an item.
+              setState(() {
+                dropdownValue = value!;
+              });
+            },
+            items: list.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
         ),
       ),
     );
--- codigo/mobile/we_budget/lib/components/forma_pagamento_dropdown.dart
+++ codigo/mobile/we_budget/lib/components/forma_pagamento_dropdown.dart
@@ -17,34 +17,36 @@ class _DropdownButtonPagamentoExample extends State<DropdownButtonPagamentoExamp
     return InputDecorator(
       decoration: InputDecoration(
         contentPadding: EdgeInsets.symmetric(
-            horizontal: 20.0, vertical: 15.0),
+            horizontal: 20.0, vertical: 7.0),
         labelText: ('Forma de pagamento'),
         border: OutlineInputBorder(
           borderSide: BorderSide(width: 0.8, color: Colors.grey), //<-- SEE HERE
           borderRadius: BorderRadius.circular(50.0),
         ),
       ),
-      child: DropdownButton<String>(
-        value: dropdownValue,
-        //icon: const Icon(Icons.arrow_downward),
-        elevation: 16,
-        //style: const TextStyle(color: Colors.deepPurple),
-        //underline: Container(
-        // height: 2,
-        //color: Colors.deepPurpleAccent,
-        //),
-        onChanged: (String? value) {
-          // This is called when the user selects an item.
-          setState(() {
-            dropdownValue = value!;
-          });
-        },
-        items: list.map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-        }).toList(),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+          value: dropdownValue,
+          //icon: const Icon(Icons.arrow_downward),
+          elevation: 16,
+          //style: const TextStyle(color: Colors.deepPurple),
+          //underline: Container(
+          // height: 2,
+          //color: Colors.deepPurpleAccent,
+          //),
+          onChanged: (String? value) {
+            // This is called when the user selects an item.
+            setState(() {
+              dropdownValue = value!;
+            });
+          },
+          items: list.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
