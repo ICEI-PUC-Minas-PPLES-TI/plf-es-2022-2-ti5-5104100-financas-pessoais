67fabb74838bcb51e54dc13be31d59b7cad34f4d
Nataniel Geraldo Mendes Peixoto <nataniel.geraldo@yahoo.com.br>
2022-11-11 21:24:39 -0300

    Ajustes graficos

--- codigo/mobile/we_budget/lib/components/sector.dart
+++ codigo/mobile/we_budget/lib/components/sector.dart
@@ -1,6 +1,11 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:provider/provider.dart';
+import 'package:we_budget/Repository/transaction_repository.dart';
+
+import '../models/transactions.dart';
 
 class Sector {
   final Color color;
@@ -9,7 +14,6 @@ class Sector {
 
   Sector({required this.color, required this.value, required this.title});
 }
-
 
 List<double> get randomNumbers {
   final Random random = Random();
@@ -21,34 +25,22 @@ List<double> get randomNumbers {
   return randomNumbers;
 }
 
-List<Sector> get industrySectors {
-  return [
-    Sector(
-        color: Colors.redAccent,
-        value: randomNumbers[0],
-        title: 'Alimentação'),
-    Sector(
-        color: Colors.blueGrey,
-        value: randomNumbers[1],
-        title: 'Gasolina'),
-    Sector(
-        color: Colors.deepPurpleAccent,
-        value: randomNumbers[2],
-        title: 'Lazer'),
-    Sector(
-        color: Colors.yellow,
-        value: randomNumbers[3],
-        title: 'Contas básicas'),
-    Sector(
-        color: Colors.green,
-        value: randomNumbers[4],
-        title: 'Pets'),
-    Sector(
-        color: Colors.orange,
-        value: randomNumbers[5],
-        title: 'Saúde'),
-    Sector(color: Colors.teal,
-        value: randomNumbers[6],
-        title: 'Imposto'),
-  ];
-}
+// List<Sector> get industrySectors(List<TransactionModel> transactions) {
+//   print("Tete agora 21:13");
+//   print(transactions);
+  
+//   return [
+//     Sector(
+//         color: Colors.redAccent, value: randomNumbers[0], title: 'Alimentação'),
+//     Sector(color: Colors.blueGrey, value: randomNumbers[1], title: 'Gasolina'),
+//     Sector(
+//         color: Colors.deepPurpleAccent,
+//         value: randomNumbers[2],
+//         title: 'Lazer'),
+//     Sector(
+//         color: Colors.yellow, value: randomNumbers[3], title: 'Contas básicas'),
+//     Sector(color: Colors.green, value: randomNumbers[4], title: 'Pets'),
+//     Sector(color: Colors.orange, value: randomNumbers[5], title: 'Saúde'),
+//     Sector(color: Colors.teal, value: randomNumbers[6], title: 'Imposto'),
+//   ];
+// }
